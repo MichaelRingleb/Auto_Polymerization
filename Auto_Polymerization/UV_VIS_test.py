b674967797a98743b25b8dc90a819b510e9a8c11
Han <clascclon@gmail.com>
2025-06-18 21:23:54 -0400

    changed pump in UV_VIS_test and also the velocity for the pump, when restarted

--- Auto_Polymerization/UV_VIS_test.py
+++ Auto_Polymerization/UV_VIS_test.py
@@ -23,7 +23,7 @@ def plot_spectrum(wavelengths, spectrum, title):
     #plt.show(block=False)  # show non-blocking so the loop can continue
 
 def pump_spectrum_loop():
-    pump = LongerPeristalticPump(com_port="COM15", address=1)
+    pump = LongerPeristalticPump(com_port="COM16", address=1)
     spec = CCSSpectrometer(
         usb_port="USB",
         device_model="CCS200",
@@ -48,7 +48,7 @@ def pump_spectrum_loop():
             plot_spectrum(wavelengths, spectrum, f"Spectrum {i} ({now.strftime('%Y-%m-%d %H:%M:%S')})")
             i += 1
             print("Restarting pump...")
-            pump.set_pump(rpm=10, on=True, direction=True)
+            pump.set_pump(rpm=0.7, on=True, direction=True)
             time.sleep(2)  # short wait before next loop
     except KeyboardInterrupt:
         print("Loop interrupted by user. Stopping pump and closing spectrometer.")
