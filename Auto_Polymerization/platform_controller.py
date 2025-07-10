a0a5b56ca862c93bfb91b62244f55229a61fd5ba
Michael Ringleb <michael.ringleb@uni-jena.de>
2025-07-10 15:43:09 -0400

    put test into platform controller

--- Auto_Polymerization/platform_controller.py
+++ Auto_Polymerization/platform_controller.py
@@ -41,8 +41,45 @@ medusa = Medusa(
     logger=logger     
 )
 
+#test code to check the functionality of the different devices
+medusa.write_serial("COM12", "b"1000\n")
+time.sleep(10)
+medusa.write_serial("COM12", "b"1000\n")
+time.sleep(10)
+medusa.write_serial("COM12", "b"GAS_ON\n")
+time.sleep(10)
+medusa.write_serial("COM12", "b"GAS_OFF\n")
+time.sleep(10)
+medusa.write_serial("COM12", "b"PRECIP_ON\n")
+time.sleep(10)
+medusa.write_serial("COM12", "b"PRECIP_OFF\n")
 
 
+medusa.heat_stir(vessel="Reaction_Vial", temperature= 20, rpm= 200)
+medusa.get_hotplate_temperature("Reaction_Vial")
+medusa.get_hotplate_rpm("Reaction_Vial")
+time.sleep(10)
+medusa.heat_stir(vessel="Reaction_Vial", temperature= 0, rpm= 0)
+time.sleep(10)
+medusa.get_hotplate_temperature("Reaction_Vial")
+medusa.get_hotplate_rpm("Reaction_Vial")
+time.sleep(10)
+
+
+medusa.transfer_volumetric(source="Purge_Solvent_Vessel_1", target="Waste_Vessel", pump_id="Solvent_Monomer_Modification_Pump", volume= 1, transfer_type="liquid")
+medusa.transfer_volumetric(source="Purge_Solvent_Vessel_2", target="Waste_Vessel", pump_id="Analytical_Pump", volume= 1, transfer_type="liquid")
+medusa.transfer_volumetric(source="Purge_Solvent_Vessel_1", target="Waste_Vessel", pump_id="Precipitation_Pump", volume= 1, transfer_type="liquid")
+medusa.transfer_volumetric(source="Purge_Solvent_Vessel_1", target="Waste_Vessel", pump_id="Initiator_CTA_Pump", volume= 1, transfer_type="liquid")
+
+
+medusa.transfer_continuous(source="Reaction_Vial", target="Reaction_Vial", pump_id="Polymer_Peri_Pump", direction_CW = True, transfer_rate=0.7)
+medusa.transfer_continuous(source="Elution_Solvent_Vessel", target="Waste_Vessel", pump_id="Solvent_Peri_Pump", direction_CW = False, transfer_rate=0.7)
+time.sleep(10)
+medusa.transfer_continuous(source="Reaction_Vial", target="Reaction_Vial", pump_id="Polymer_Peri_Pump", direction_CW = True, transfer_rate=0.0)
+medusa.transfer_continuous(source="Elution_Solvent_Vessel", target="Waste_Vessel", pump_id="Solvent_Peri_Pump", direction_CW = False, transfer_rate=0.0)
+
+
+exit*()
 """
 #ideally put into its own module, but for now just import here
 # Definition of added volumes and reaction temperature by user before reaction:
