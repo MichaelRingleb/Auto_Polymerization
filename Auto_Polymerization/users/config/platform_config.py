7aee3413c257b41ab48506379fc77568f30f1690
Michael Ringleb <michael.ringleb@uni-jena.de>
2025-07-20 15:23:55 -0400

    reconfigured the config.py to make it more understandable

--- Auto_Polymerization/users/config/platform_config.py
+++ Auto_Polymerization/users/config/platform_config.py
@@ -15,189 +15,189 @@ If you change tubing lengths, vessel sizes, or hardware, update the relevant vol
 Do not rename or remove keys unless you are also updating the workflow code.
 """
 
+# -------------------------------------------------------------------
+# GENERAL EXPERIMENT METADATA & PATHS
+# -------------------------------------------------------------------
+experiment_id = "MRG_061_I"  # Unique experiment identifier
+data_base_path = "users/data"  # Base directory for all experiment data
+nmr_data_base_path = "users/data/NMR_data"  # Directory for NMR data (subfolder of data_base_path)
 
-# Experiment ID and base path for data storage
-experiment_id= "MRG_061_I"
-data_base_path = "users/data"
-nmr_data_base_path = "users/data/NMR_data"  # Specific NMR data directory
-
-
-#Parameters for the preparation workflow substep, change, if you have longer lines, which are not sufficiently primed with the current volumes 
-prime_transfer_params = { 
-    "prime_volume": 1,
-    "draw_speed": 0.07,
-    "dispense_speed": 0.1,
-    "flush": 1,
-    "flush_volume": 5,
-    "flush_speed": 0.2,
-    "pre_rinse": 2,
-    "pre_rinse_volume": 0.5,
-    "pre_rinse_speed": 0.1,
-    "post_rinse": 1,
-    "post_rinse_volume": 2.5,
-    "post_rinse_speed": 0.1,
+# -------------------------------------------------------------------
+# PREPARATION WORKFLOW PARAMETERS
+# -------------------------------------------------------------------
+# Priming and cleaning parameters for all lines and pumps
+prime_transfer_params = {
+    "prime_volume": 1,           # mL, volume for priming
+    "draw_speed": 0.07,          # mL/s, speed for drawing liquid
+    "dispense_speed": 0.1,       # mL/s, speed for dispensing liquid
+    "flush": 1,                  # times, number of flushes
+    "flush_volume": 5,           # mL, volume per flush
+    "flush_speed": 0.2,          # mL/s, speed for flushing
+    "pre_rinse": 2,              # times, number of pre-rinses
+    "pre_rinse_volume": 0.5,     # mL, volume per pre-rinse
+    "pre_rinse_speed": 0.1,      # mL/s, speed for pre-rinse
+    "post_rinse": 1,             # times, number of post-rinses
+    "post_rinse_volume": 2.5,    # mL, volume per post-rinse
+    "post_rinse_speed": 0.1,     # mL/s, speed for post-rinse
 }
 
-#Parameters for the polymerization workflow substep, change, if you want to use different volumes for the reaction components
-polymerization_params = { 
-    "solvent_volume": 10,
-    "solvent_draw_speed": 0.08,
-    "solvent_dispense_speed": 0.13,
-    "monomer_volume": 4,
-    "monomer_draw_speed": 0.08,
-    "monomer_dispense_speed": 0.13,
-    "cta_volume": 4,
-    "cta_draw_speed": 0.08,
-    "cta_dispense_speed": 0.13,
-    "initiator_volume": 3,
-    "initiator_draw_speed": 0.08,
-    "initiator_dispense_speed": 0.13,
-    "flush": 2,
-    "flush_volume": 5,
-    "flush_speed": 0.3,
-    "pre_rinse": 1,
-    "pre_rinse_volume": 0.5,
-    "pre_rinse_speed": 0.1,
-    "post_rinse": 1,
-    "post_rinse_volume": 2.5,
-    "last_post_rinse_volume": 6,
-    "post_rinse_speed": 0.133,
-    "post_rinse_vessel": "Purge_Solvent_Vessel_1",
+# -------------------------------------------------------------------
+# POLYMERIZATION WORKFLOW PARAMETERS
+# -------------------------------------------------------------------
+# Volumes and speeds for each reagent/component
+polymerization_params = {
+    "solvent_volume": 10,            # mL
+    "solvent_draw_speed": 0.08,      # mL/s
+    "solvent_dispense_speed": 0.13,  # mL/s
+    "monomer_volume": 4,             # mL
+    "monomer_draw_speed": 0.08,      # mL/s
+    "monomer_dispense_speed": 0.13,  # mL/s
+    "cta_volume": 4,                 # mL
+    "cta_draw_speed": 0.08,          # mL/s
+    "cta_dispense_speed": 0.13,      # mL/s
+    "initiator_volume": 3,           # mL
+    "initiator_draw_speed": 0.08,    # mL/s
+    "initiator_dispense_speed": 0.13,# mL/s
+    "flush": 2,                      # times
+    "flush_volume": 5,               # mL
+    "flush_speed": 0.3,              # mL/s
+    "pre_rinse": 1,                  # times
+    "pre_rinse_volume": 0.5,         # mL
+    "pre_rinse_speed": 0.1,          # mL/s
+    "post_rinse": 1,                 # times
+    "post_rinse_volume": 2.5,        # mL
+    "last_post_rinse_volume": 6,     # mL, final rinse
+    "post_rinse_speed": 0.133,       # mL/s
+    "post_rinse_vessel": "Purge_Solvent_Vessel_1", # Vessel for post-rinse
 }
 
-# Timing and workflow-specific parameters (seconds, iterations, etc.)
+# -------------------------------------------------------------------
+# TIMINGS FOR WORKFLOW STEPS (seconds unless otherwise noted)
+# -------------------------------------------------------------------
 timings = {
-    "deoxygenation_time": 1200, #20 min
-    "functionalization_interval_sec": 180,  # 3 minutes
-    "functionalization_max_iterations": 200,
-    "precipitation_wait_sec": 600,
+    "deoxygenation_time": 1200,                # s, time for deoxygenation (20 min)
+    "functionalization_interval_sec": 180,     # s, interval between functionalization measurements (3 min)
+    "functionalization_max_iterations": 200,   # max iterations for functionalization (10 hours)
+    "precipitation_wait_sec": 600,             # s, wait time for precipitation (10 min)
 }
 
-
 # -------------------------------------------------------------------
-# Polymerization Monitoring Parameters
+# POLYMERIZATION MONITORING & DIALYSIS MONITORING PARAMETERS
+# -------------------------------------------------------------------
 # Used for both polymerization and dialysis shimming intervals, etc.
-# -------------------------------------------------------------------
-# Polymerization monitoring parameters
 polymerization_monitoring_params = {
-    "nmr_monomer_region": (5.0, 6.0),  # ppm range for monomer peaks
-    "nmr_standard_region": (6.5, 7.5),  # ppm range for internal standard peaks
-    "nmr_noise_region": (9.0, 10.0),  # ppm range for baseline noise calculation
-    "nmr_scans": 32,  # Number of NMR scans per measurement
-    "nmr_spectrum_center": 5,  # ppm center of NMR spectrum
-    "nmr_spectrum_width": 12,  # ppm width of NMR spectrum
-    "measurement_interval_minutes": 10,  # Time between measurements
-    "shimming_interval": 4,  # Reshim every N measurements
-    "conversion_threshold": 80,  # Stop at this conversion %
-    "max_monitoring_hours": 20,  # Maximum monitoring time, after this time the workflow will stop and continue with the next step
-}
-
-
-
-# Temperatures for various steps (C)
-temperatures = {
-    "polymerization_temp": 75,
-    "functionalization_temp": 30,
-    "cleaning_dry_temp": 80,
+    "nmr_monomer_region": (5.0, 6.0),      # ppm, monomer peak region
+    "nmr_standard_region": (6.5, 7.5),     # ppm, internal standard region
+    "nmr_noise_region": (9.0, 10.0),       # ppm, baseline noise region
+    "nmr_scans": 32,                       # scans per measurement
+    "nmr_spectrum_center": 5,              # ppm, spectrum center
+    "nmr_spectrum_width": 12,              # ppm, spectrum width
+    "measurement_interval_minutes": 10,     # min, time between measurements
+    "shimming_interval": 4,                # reshim every N measurements
+    "conversion_threshold": 80,            # %, stop at this conversion
+    "max_monitoring_hours": 20,            # h, max monitoring time
 }
 
 # -------------------------------------------------------------------
-# Dialysis Workflow Parameters
-# Only dialysis-specific booleans and references to shared config values.
+# TEMPERATURES FOR VARIOUS STEPS (°C)
+# -------------------------------------------------------------------
+temperatures = {
+    "polymerization_temp": 75,         # °C, polymerization
+    "functionalization_temp": 30,      # °C, modification/functionalization
+    "cleaning_dry_temp": 80,           # °C, cleaning
+}
+
+# -------------------------------------------------------------------
+# DIALYSIS WORKFLOW PARAMETERS
 # -------------------------------------------------------------------
 dialysis_params = {
     "noise_comparison_based": True,      # If True, stop dialysis when monomer peak < 3x noise (NMR-based)
-    "time_based": True,            # If True, stop dialysis after a set duration (see below)
-    "dialysis_duration_mins": 180, # Duration (in minutes) for time-based stopping
-    "dialysis_measurement_interval_minutes": None,  # If set, overrides monitoring interval for dialysis. If None, uses polymerization_monitoring_params["measurement_interval_minutes"].
+    "time_based": True,                  # If True, stop dialysis after a set duration (see below)
+    "dialysis_duration_mins": 180,       # min, duration for time-based stopping
+    "dialysis_measurement_interval_minutes": None,  # min, overrides monitoring interval if set
     # The following parameters are referenced from other config dicts:
     # "sample_volume_ml": nmr_transfer_params['sample_volume_ml']
     # "reshim_interval": polymerization_monitoring_params['shimming_interval']
     # NMR regions for dialysis are the same as for monitoring.
 }
 
-# RPM settings for various steps
+# -------------------------------------------------------------------
+# RPM SETTINGS FOR VARIOUS STEPS
+# -------------------------------------------------------------------
 target_rpm = {
-    "polymerization_rpm": 600,
-    "cleaning_rpm": 300,
+    "polymerization_rpm": 600,   # rpm, polymerization
+    "cleaning_rpm": 300,         # rpm, cleaning
 }
+
 # -------------------------------------------------------------------
-# NMR Transfer Parameters
-# Used for all NMR sample transfers (monitoring, t0, dialysis, etc.)
+# NMR TRANSFER PARAMETERS (used for all NMR sample transfers)
 # -------------------------------------------------------------------
-# Parameters for the analytical transfers (NMR shimming, NMR sampling, UV-VIS)
-#change if you changed the length of the tubing or the volume of the NMR cell 
+# Change if you changed the length of the tubing or the volume of the NMR cell
 nmr_transfer_params = {
     # Parameters for NMR shimming transfer (deuterated solvent)
     "shimming": {
-        "volume": 2.1,
-        "draw_speed": 0.07,
-        "dispense_speed": 0.07,
-        "flush": 1,
-        "flush_volume": 5,
-        "flush_speed": 0.15,
-        "post_rinse": 1,
-        "post_rinse_volume": 1.5,
-        "post_rinse_speed": 0.1,
+        "volume": 2.1,                # mL
+        "draw_speed": 0.07,           # mL/s
+        "dispense_speed": 0.07,       # mL/s
+        "flush": 1,                   # times
+        "flush_volume": 5,            # mL
+        "flush_speed": 0.15,          # mL/s
+        "post_rinse": 1,              # times
+        "post_rinse_volume": 1.5,     # mL
+        "post_rinse_speed": 0.1,      # mL/s
         "post_rinse_vessel": "Purge_Solvent_Vessel_2",
         "transfer_type": "liquid"
     },
     # Parameters for NMR sampling transfer (reaction/sample)
     "sampling": {
-        "volume": 2.1,
-        "draw_speed": 0.05,
-        "dispense_speed": 0.05,"flush": 1,
-        "flush_volume": 5,
-        "flush_speed": 0.15,
-        "post_rinse": 1,
-        "post_rinse_volume": 2.5,
-        "post_rinse_speed": 0.1,
+        "volume": 2.1,                # mL
+        "draw_speed": 0.05,           # mL/s
+        "dispense_speed": 0.05,       # mL/s
+        "flush": 1,                   # times
+        "flush_volume": 5,            # mL
+        "flush_speed": 0.15,          # mL/s
+        "post_rinse": 1,              # times
+        "post_rinse_volume": 2.5,     # mL
+        "post_rinse_speed": 0.1,      # mL/s
         "post_rinse_vessel": "Purge_Solvent_Vessel_2",
         "transfer_type": "liquid"
     }
 }
-#change if you changed the length of the tubing to the UV-VIS cell
+
+# -------------------------------------------------------------------
+# UV-VIS TRANSFER PARAMETERS (change if you changed tubing length to UV-VIS cell)
+# -------------------------------------------------------------------
 uv_vis_transfer_params = {
-    "volume": 1.5,
-    "draw_speed": 0.03,
-    "dispense_speed": 0.016,
-    "post_rinse": 1,
-    "post_rinse_volume": 1.5,
-    "post_rinse_speed": 0.1,
+    "volume": 1.5,                # mL
+    "draw_speed": 0.03,           # mL/s
+    "dispense_speed": 0.016,      # mL/s
+    "post_rinse": 1,              # times
+    "post_rinse_volume": 1.5,     # mL
+    "post_rinse_speed": 0.1,      # mL/s
     "post_rinse_vessel": "Purge_Solvent_Vessel_2",
     "transfer_type": "liquid"
 }
 
-
-
-
-
-# Default volumes for different transfers / components
+# -------------------------------------------------------------------
+# DEFAULT VOLUMES FOR COMMON TRANSFERS/COMPONENTS
+# -------------------------------------------------------------------
 default_volumes = {
-    "prime" : 2,
-    "solvent": 10,
-    "monomer": 4,
-    "initiator": 3,
-    "cta": 4,
-    "modification": 2,
-    "nmr": 2.1,
-    "uv_vis": 1.5,
-    "flush": 5,
-    "functionalization": 2,
-    "precipitation_methanol": 25,
-    "precipitation_argon": 100,
-    "cleaning_purge_solvent": 30,
-    "cleaning_dry_argon": 200
-    
+    "prime": 2,                       # mL
+    "solvent": 10,                    # mL
+    "monomer": 4,                     # mL
+    "initiator": 3,                   # mL
+    "cta": 4,                         # mL
+    "modification": 2,                # mL
+    "nmr": 2.1,                       # mL
+    "uv_vis": 1.5,                    # mL
+    "flush": 5,                       # mL
+    "functionalization": 2,           # mL
+    "precipitation_methanol": 25,     # mL
+    "precipitation_argon": 100,       # mL
+    "cleaning_purge_solvent": 30,     # mL
+    "cleaning_dry_argon": 200,        # mL
 }
 
-
-
-
-
-
-
-
-
-
-# Add more user-editable parameters as needed below 
+# -------------------------------------------------------------------
+# Add more user-editable parameters as needed below
+# ------------------------------------------------------------------- 
